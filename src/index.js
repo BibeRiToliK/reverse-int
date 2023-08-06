a67845cdee72eb236f3e1f5e40ed2a6a2ec8aec4
Uladzimir Piskunovich <vova.piskunovich2001@gmail.com>
2023-08-06 10:42:25 +0300

    end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+   let nStr = n.toString();
+   let res = ""
+   for (i = nStr.length; i !== 0; i--) {
+    res += nStr[i-1];
+ }
+    return parseInt(res)
+};
